07dbcde984000a52f4e6a9954f225598f4036eb1
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2022-11-14 19:39:36 +0200

    JS Full Lessons

---
+++ lesson-13-modules-and-unit-tests/hw2/calculator.test.js
@@ -0,0 +1,31 @@
+import { calc } from "./calculator.js";
+
+it("should get sum of numbers", () => {
+  const result = calc("4 + 3");
+
+  expect(result).toEqual("4 + 3 = 7");
+});
+
+it("should keep difference of numbers", () => {
+  const result = calc("11 - 2");
+
+  expect(result).toEqual("11 - 2 = 9");
+});
+
+it("should get multiplication of numbers", () => {
+  const result = calc("5 * 5");
+
+  expect(result).toEqual("5 * 5 = 25");
+});
+
+it("should get division of numbers", () => {
+  const result = calc("49 / 7");
+
+  expect(result).toEqual("49 / 7 = 7");
+});
+
+it("should get null if typeof expression is not string", () => {
+  const result = calc(4 + 3);
+
+  expect(result).toEqual(null);
+});
