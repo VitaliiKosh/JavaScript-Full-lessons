4f5d8b389fd81b3e93aeb502be31d1e9f99c8a80
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2022-11-30 01:22:11 +0200

    DOM

---
+++ lesson-21-DOM/hw8/index.js
@@ -0,0 +1,22 @@
+export function finishList() {
+  const newElem = document.querySelector(".list");
+  const newSecondElem = document.querySelector(".special");
+
+  const firstElem = document.createElement("li");
+  firstElem.textContent = "1";
+  newElem.prepend(firstElem);
+
+  const fourthElem = document.createElement("li");
+  fourthElem.textContent = "4";
+  newSecondElem.before(fourthElem);
+
+  const sixthElem = document.createElement("li");
+  sixthElem.textContent = "6";
+  newSecondElem.after(sixthElem);
+
+  const eigthElem = document.createElement("li");
+  eigthElem.textContent = "8";
+  newElem.append(eigthElem);
+}
+
+finishList();
