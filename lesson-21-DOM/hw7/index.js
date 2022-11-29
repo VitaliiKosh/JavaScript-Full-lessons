3dce3f59d70bc8304209ba5252d52769e4ac23cc
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2022-11-30 00:49:07 +0200

    DOM

--- lesson-21-DOM/hw7/index.js
+++ lesson-21-DOM/hw7/index.js
@@ -1,4 +1,4 @@
-function createButton(buttonText) {
+export function createButton(buttonText) {
   const newElem = document.querySelector("body");
   const newButton = document.createElement("button");
   newButton.textContent = `${buttonText}`;
