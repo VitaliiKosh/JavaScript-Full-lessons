b9b8e2f564db8801d5418c4606bdc1dc6cc000bf
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2022-11-29 19:42:07 +0200

    DOM

--- lesson-21-DOM/hw1/index.js
+++ lesson-21-DOM/hw1/index.js
@@ -7,7 +7,7 @@ export function getTitleElement() {
 getTitleElement();
 
 export function getInputElement() {
-  const inputElem = document.querySelector("input.text");
+  const inputElem = document.querySelector("input");
   console.dir(inputElem);
   return inputElem;
 }
