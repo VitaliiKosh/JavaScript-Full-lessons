0d110ceba3032e0bca4589dce6bfbb0a6f0d081a
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2022-11-30 00:48:03 +0200

    DOM

---
+++ lesson-21-DOM/hw7/index.js
@@ -0,0 +1,8 @@
+function createButton(buttonText) {
+  const newElem = document.querySelector("body");
+  const newButton = document.createElement("button");
+  newButton.textContent = `${buttonText}`;
+  newElem.append(newButton);
+}
+
+// createButton("Button Start");
