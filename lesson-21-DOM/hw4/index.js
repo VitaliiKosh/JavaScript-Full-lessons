c0513e0a801c7fff86cd8a10f68ca1005de6d17c
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2022-11-29 23:18:39 +0200

    DOM

--- lesson-21-DOM/hw4/index.js
+++ lesson-21-DOM/hw4/index.js
@@ -1,7 +1,7 @@
 export function setTitle(text) {
   const titleNewText = document.querySelector(".title");
   titleNewText.textContent = text;
-  return titleNewText.textContent;
+  //  return titleNewText.textContent;
 }
 
 setTitle("I will be very good Front-End developer!");
