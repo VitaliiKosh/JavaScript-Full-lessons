411d10034c7e5b56739f825e5c34b49ed8ecec17
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2022-11-29 22:55:55 +0200

    DOM

--- lesson-21-DOM/hw6/index.js
+++ lesson-21-DOM/hw6/index.js
@@ -2,7 +2,6 @@ export function clearList() {
   const clear = document.querySelector(".categories");
   const clearContent = clear.innerHTML;
   clear.innerHTML = "";
-  // console.dir(clearContent);
   return clearContent;
 }
 
