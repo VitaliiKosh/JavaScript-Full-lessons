2e873649edd603c6968be510a292a6746f2a0aa0
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2022-11-30 21:07:21 +0200

    DOM

---
+++ lesson-21-DOM/hw11/index.js
@@ -0,0 +1,12 @@
+function squaredNumbers() {
+  const arr = Array.from(document.querySelectorAll(".number"));
+  console.log(arr);
+
+  arr.map((arrElem) => {
+    const searchNumber = arrElem.querySelector("data-number");
+    const numberOfData = Object.values(searchNumber.dataset);
+    searchNumber.dataset.squaredNumber = numberOfData[0] ** 2;
+  });
+}
+
+squaredNumbers();
