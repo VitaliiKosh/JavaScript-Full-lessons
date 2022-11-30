76e0877d9f47ce0b10c92a55f2c4908a7aa68390
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2022-11-30 18:05:57 +0200

    DOM

---
+++ lesson-21-DOM/hw10/index.js
@@ -0,0 +1,18 @@
+export function manageClasses() {
+  const elementOne = document.querySelector(".one");
+  const elementTwo = document.querySelector(".two");
+  const elementThree = document.querySelector(".three");
+  const elementFour = document.querySelector(".four");
+
+  elementOne.classList.add("selected");
+
+  elementTwo.classList.remove("selected");
+
+  elementThree.classList.toggle("three_done");
+
+  if (elementFour.classList.contains("some-class")) {
+    elementFour.classList.add("another-class");
+  }
+}
+
+// manageClasses();
