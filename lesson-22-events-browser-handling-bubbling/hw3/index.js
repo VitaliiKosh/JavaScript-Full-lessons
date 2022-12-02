8e4055a90d0bfdade8b01ee7312665894df655c8
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2022-12-02 06:18:54 +0200

    Events

--- lesson-22-events-browser-handling-bubbling/hw3/index.js
+++ lesson-22-events-browser-handling-bubbling/hw3/index.js
@@ -2,6 +2,7 @@ const checkboxElem = document.querySelector(".task-status");
 
 function handleClick() {
   console.log(!!checkboxElem.checked);
+  //  console.log(checkboxElem.value);
 }
 
 checkboxElem.addEventListener("change", handleClick);
