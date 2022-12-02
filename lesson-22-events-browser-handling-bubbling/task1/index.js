b274edca8657c9380b6116a7cacc5dcf9e57ca2e
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2022-12-02 22:55:04 +0200

    Events

--- lesson-22-events-browser-handling-bubbling/task1/index.js
+++ lesson-22-events-browser-handling-bubbling/task1/index.js
@@ -23,6 +23,59 @@ const logGreyDiv = logTarget.bind(null, "DIV", "grey");
 const logGreyP = logTarget.bind(null, "P", "grey");
 const logGreySpan = logTarget.bind(null, "SPAN", "grey");
 
+divElem.addEventListener("click", logGreyDiv, true);
+divElem.addEventListener("click", logGreenDiv);
+
+pElem.addEventListener("click", logGreyP, true);
+pElem.addEventListener("click", logGreenP);
+
+spanElem.addEventListener("click", logGreySpan, true);
+spanElem.addEventListener("click", logGreenSpan);
+
+function removeElement() {
+  divElem.removeEventListener("click", logGreyDiv, true);
+  divElem.removeEventListener("click", logGreenDiv);
+
+  pElem.removeEventListener("click", logGreyP, true);
+  pElem.removeEventListener("click", logGreenP);
+
+  spanElem.removeEventListener("click", logGreySpan, true);
+  spanElem.removeEventListener("click", logGreenSpan);
+}
+removeBtn.addEventListener("click", removeElement);
+
+function clearElement() {
+  // outputField.reset();
+  outputField.innerHTML = `${""}`;
+}
+clearBtn.addEventListener("click", clearElement);
+
+/*
+const divElem = document.querySelector(".rect_div");
+const pElem = document.querySelector(".rect_p");
+const spanElem = document.querySelector(".rect_span");
+
+const clearBtn = document.querySelector(".clear-btn");
+const removeBtn = document.querySelector(".remove-handlers-btn");
+const attachBtn = document.querySelector(".attach-handlers-btn");
+
+const outputField = document.querySelector(".events-list");
+// outputField.innerHTML = `${""}`;
+
+const logTarget = (text, color) => {
+  const eventsListElem = document.querySelector(".events-list");
+
+  eventsListElem.innerHTML += `<span style="color: ${color}; margin-left: 8px;">${text}</span>`;
+};
+
+const logGreenDiv = logTarget.bind(null, "DIV", "green");
+const logGreenP = logTarget.bind(null, "P", "green");
+const logGreenSpan = logTarget.bind(null, "SPAN", "green");
+
+const logGreyDiv = logTarget.bind(null, "DIV", "grey");
+const logGreyP = logTarget.bind(null, "P", "grey");
+const logGreySpan = logTarget.bind(null, "SPAN", "grey");
+
 function addElement() {
   divElem.addEventListener("click", logGreyDiv, true);
   divElem.addEventListener("click", logGreenDiv);
@@ -52,3 +105,4 @@ function clearElement() {
   outputField.innerHTML = `${""}`;
 }
 clearBtn.addEventListener("click", clearElement);
+*/
