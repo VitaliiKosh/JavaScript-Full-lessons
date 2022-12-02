b52b937c22430fe308aa1d8064bf08c1232559a4
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2022-12-02 20:06:49 +0200

    Events

---
+++ lesson-22-events-browser-handling-bubbling/hw5/index.js
@@ -0,0 +1,8 @@
+const buttonElem = document.querySelector(".single-use-btn");
+
+function handleClick() {
+  console.log("clicked");
+  buttonElem.removeEventListener("click", handleClick);
+}
+
+buttonElem.addEventListener("click", handleClick);
