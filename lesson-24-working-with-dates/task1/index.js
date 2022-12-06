6fd60a93cebaf3964e84cf037a77920a18a15349
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2022-12-06 22:47:30 +0200

    Date

--- lesson-24-working-with-dates/task1/index.js
+++ lesson-24-working-with-dates/task1/index.js
@@ -1,6 +1,6 @@
 const weekDays = ["Mo", "Tu", "We", "Th", "Fr", "St", "Su"];
 
-export const dayOfWeek = (date, days) => {
+const dayOfWeek = (date, days) => {
   const day = new Date(date).getDate();
   const dateInFuture = new Date(date).setDate(day + days);
 
