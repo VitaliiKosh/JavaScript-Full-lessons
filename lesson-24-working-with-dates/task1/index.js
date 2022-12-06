205c6ce59aa21d189d57af539aa3fd2faae30863
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2022-12-06 22:49:08 +0200

    Date

--- lesson-24-working-with-dates/task1/index.js
+++ lesson-24-working-with-dates/task1/index.js
@@ -1,6 +1,6 @@
 const weekDays = ["Mo", "Tu", "We", "Th", "Fr", "St", "Su"];
 
-const dayOfWeek = (date, days) => {
+export const dayOfWeek = (date, days) => {
   const day = new Date(date).getDate();
   const dateInFuture = new Date(date).setDate(day + days);
 
@@ -8,13 +8,13 @@ const dayOfWeek = (date, days) => {
 };
 
 const result = dayOfWeek(new Date(2022, 11, 6), 3);
-const result1 = dayOfWeek(new Date(2022, 11, 6), 5);
-const result2 = dayOfWeek(new Date(2022, 11, 6), 7);
-const result3 = dayOfWeek(new Date(2022, 11, 6), 1);
-const result4 = dayOfWeek(new Date(2022, 11, 6), 2);
+// const result1 = dayOfWeek(new Date(2022, 11, 6), 5);
+// const result2 = dayOfWeek(new Date(2022, 11, 6), 7);
+// const result3 = dayOfWeek(new Date(2022, 11, 6), 1);
+// const result4 = dayOfWeek(new Date(2022, 11, 6), 2);
 
 console.log(result);
-console.log(result1);
-console.log(result2);
-console.log(result3);
-console.log(result4);
+// console.log(result1);
+// console.log(result2);
+// console.log(result3);
+// console.log(result4);
