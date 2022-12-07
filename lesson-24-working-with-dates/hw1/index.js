56f5ebaff70a67ca5110c4fbcad9e63bde8d702f
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2022-12-07 08:58:29 +0200

    Date

--- lesson-24-working-with-dates/hw1/index.js
+++ lesson-24-working-with-dates/hw1/index.js
@@ -1,8 +1,10 @@
 export const getDiff = (startDate, endDate) => {
   const newStartDate = new Date(startDate).getTime();
+  // console.log(newStartDate);
   const newEndDay = new Date(endDate).getTime();
-  const diffDate = Math.abs(newStartDate - newEndDay);
-  // console.log(diffDate); // ==> 82176645999
+  // console.log(newEndDay);
+  const diffDate = Math.abs(Math.abs(newStartDate) - Math.abs(newEndDay));
+  console.log(diffDate); // ==> 82176645999
   // console.log(new Date(diffDate)); // ==> Wed Aug 09 1972 05:50:45 GMT+0300 (Восточная Европа, стандартное время)
   // 86400000ms in 1 day, 3600000ms in 1 hour, 60000ms in 1 minute, 1000ms in 1 second
 
@@ -21,8 +23,12 @@ export const getDiff = (startDate, endDate) => {
 
   console.log(`${days}d ${hours}h ${minutes}m ${seconds}s`);
 };
-
+/*
 const startDate = new Date(Date.UTC(2022, 11, 7, 7, 46, 64, 44));
+const endDate = new Date(Date.UTC(2020, 3, 30, 4, 56, 18, 45));
+*/
+
+const startDate = new Date(Date.UTC(1956, 20, 5, 7, 46, 64, 44));
 const endDate = new Date(Date.UTC(2020, 3, 30, 4, 56, 18, 45));
 
 getDiff(startDate, endDate);
