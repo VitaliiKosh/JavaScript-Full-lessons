f030ec11464f68617b16d6cef70ec2ac1fac2c3d
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2022-12-07 08:46:13 +0200

    Date

--- lesson-24-working-with-dates/hw1/index.js
+++ lesson-24-working-with-dates/hw1/index.js
@@ -1,12 +1,28 @@
-const startDate = new Date(Date.UTC(2019, 10, 24, 20, 50, 34, 44));
-const endDate = new Date(Date.UTC(2019, 1, 22, 17, 52, 18));
+const getDiff = (startDate, endDate) => {
+  const newStartDate = new Date(startDate).getTime();
+  const newEndDay = new Date(endDate).getTime();
+  const diffDate = Math.abs(newStartDate - newEndDay);
+  // console.log(diffDate); // ==> 82176645999
+  // console.log(new Date(diffDate)); // ==> Wed Aug 09 1972 05:50:45 GMT+0300 (Восточная Европа, стандартное время)
+  // 86400000ms in 1 day, 3600000ms in 1 hour, 60000ms in 1 minute, 1000ms in 1 second
 
-const diffDate = Math.abs(
-  new Date(startDate).getTime() - new Date(endDate).getTime()
-);
+  const days = Math.floor(diffDate / 86400000);
+  // console.log(days); // ==> 951
+  const hours = Math.floor((diffDate - days * 86400000) / 3600000);
+  // console.log(hours); // ==> 2
+  const minutes = Math.floor(
+    (diffDate - days * 86400000 - hours * 3600000) / 60000
+  );
+  // console.log(minutes); // ==> 50
+  const seconds = Math.floor(
+    (diffDate - days * 86400000 - hours * 3600000 - minutes * 60000) / 1000
+  );
+  // console.log(seconds); // ==> 45
 
-console.log(diffDate); // ==> 23770696044
+  console.log(`${days}d ${hours}h ${minutes}m ${seconds}s`);
+};
 
-const hoursData = new Date(diffDate).getUTCHours();
+const startDate = new Date(Date.UTC(2022, 11, 7, 7, 46, 64, 44));
+const endDate = new Date(Date.UTC(2020, 3, 30, 4, 56, 18, 45));
 
-console.log(hoursData);
+getDiff(startDate, endDate);
