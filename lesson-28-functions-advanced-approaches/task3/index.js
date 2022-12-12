b0452c5982cbd83619121edb75753aaf64e95661
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2022-12-12 19:53:04 +0200

    JS Full Lessons

--- lesson-28-functions-advanced-approaches/task3/index.js
+++ lesson-28-functions-advanced-approaches/task3/index.js
@@ -1,11 +1,13 @@
 export const mult = (value1) => (value2) => value1 * value2;
 
-export const twice = mult(7);
-const result1 = twice(2);
+export const twice = mult(7)(2);
+// const result1 = twice(2);
 
-export const triple = mult(9);
-const result2 = triple(3);
+export const triple = mult(9)(3);
+// const result2 = triple(3);
 
 console.log(mult(5)(4)); // ==> 20
-console.log(result1); // ==> 14
-console.log(result2); // ==> 27
+// console.log(result1); // ==> 14
+// console.log(result2); // ==> 27
+console.log(twice); // ==> 14
+console.log(triple); // ==> 27
