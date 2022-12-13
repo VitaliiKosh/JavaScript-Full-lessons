22a798c8d387938e362af0e06704bf065fb886f4
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2022-12-13 14:18:06 +0200

    JS Full Lessons

---
+++ lesson-29-asynchronous-code/task2/index.js
@@ -0,0 +1,7 @@
+export const pinger = (count, period) => {
+  // put your code here
+};
+
+// examples
+pinger(5, 100); // makes 5 writes with 100 ms interval
+pinger(7, 150); // makes 7 writes with 1500 ms interval
---
+++ lesson-29-asynchronous-code/task3/index.js
@@ -0,0 +1,19 @@
+export const printing = () => {
+  console.log(1);
+
+  setTimeout(function () {
+    console.log(2);
+  }, 1000);
+
+  console.log(3);
+
+  setTimeout(function () {
+    console.log(4);
+  }, 0);
+
+  setTimeout(function () {
+    console.log(5);
+  }, 2000);
+
+  console.log(6);
+};
