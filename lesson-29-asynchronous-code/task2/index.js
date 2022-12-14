dd32d1960e1ab885a03921524a3f14ab40e1236c
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2022-12-15 01:25:00 +0200

    JS Full Lessons

--- lesson-29-asynchronous-code/task2/index.js
+++ lesson-29-asynchronous-code/task2/index.js
@@ -1,5 +1,14 @@
 export const pinger = (count, period) => {
   // put your code here
+  let i = count;
+  console.log("Ping");
+  const interval = setInterval(() => {
+    if (--i > 0) {
+      console.log("Ping");
+    } else {
+      clearInterval(interval);
+    }
+  }, period);
 };
 
 // examples
