a7ff2620c57c1e5b6844fd8dfb0d78eb7668327a
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2022-12-15 00:47:03 +0200

    JS Full Lessons

--- lesson-29-asynchronous-code/task1/index.js
+++ lesson-29-asynchronous-code/task1/index.js
@@ -16,8 +16,7 @@ export const addImage = (imgSrc, callback) => {
   imgElem.addEventListener("error", () => callback("Image load failed"));
 };
 
-const imgSrc =
-  "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg";
+// const imgSrc = "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg";
 
 // callack function
 const onImageLoaded = (error, imgElem) => {
@@ -32,10 +31,10 @@ const onImageLoaded = (error, imgElem) => {
   sizeElem.textContent = `${width} x ${height}`;
 };
 
-addImage(imgSrc, onImageLoaded);
+// addImage(imgSrc, onImageLoaded);
 
 // examples
-// addImage(
-//   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
-//   onImageLoaded,
-// );
+addImage(
+  "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg",
+  onImageLoaded
+);
