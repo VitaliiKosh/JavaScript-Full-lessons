5abd97547118df69db8ac9fa44e1d301fe59408b
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2022-12-22 22:28:15 +0200

    JS Full lessons

--- lesson-31-promise-handlers-chaining/task1/index.js
+++ lesson-31-promise-handlers-chaining/task1/index.js
@@ -1,3 +1,25 @@
-export const requestUserData = (userId) => {
-  // put your code here
+const requestUserData = (userId) => {
+  const request = new Promise((resolve, reject) => {
+    if (userId === "broken") {
+      setTimeout(() => {
+        reject(new Error("User not found"));
+      }, 500);
+    } else {
+      setTimeout(() => {
+        resolve({
+          name: "John",
+          age: 17,
+          userId,
+          email: `${userId}@example.com`,
+        });
+      }, 1000);
+    }
+  });
+  return request;
 };
+
+requestUserData("userid777").then((data) => console.log(data)); // {name: 'John', age: 17, userId: 'userid777', email: 'userid777@example.com'}
+
+requestUserData("broken").catch((error) => console.log(error)); // Error: User not found
+
+// requestUserData("broken-1").finally(() => console.log("finally"));
