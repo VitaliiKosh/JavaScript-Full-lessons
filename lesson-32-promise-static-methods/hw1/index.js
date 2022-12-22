2cce17664b1e7dd2c6a45a73956931a4cd8232ee
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2022-12-23 00:42:11 +0200

    JS Full lessons

---
+++ lesson-32-promise-static-methods/hw1/index.js
@@ -0,0 +1,14 @@
+/*
+ * ф-ция makePromise должна вернуть промис со значением переданным в ф-цию
+ */
+
+const makePromise = () => {
+  // put you code here
+};
+
+/*
+ * пример использования
+ */
+makePromise(17).then((number) => {
+  console.log(number); // 17
+});
