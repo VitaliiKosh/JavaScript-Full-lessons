c11d1839ba2b77ce2eba2a8bcf53c1d84de63e6d
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2022-12-23 06:16:33 +0200

    JS Full lessons

---
+++ lesson-32-promise-static-methods/task1/index.js
@@ -0,0 +1,21 @@
+const getValueWithDelay = (value, delay) =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(value);
+      resolve(value);
+    }, delay);
+  });
+
+const asyncNum1 = getValueWithDelay(56);
+const asyncNum2 = getValueWithDelay(4);
+const asyncNum3 = getValueWithDelay(10);
+
+const asyncSum = (...asyncNumbers) => {
+  return Promise.allSettled(asyncNumbers).then((numbers) =>
+    console.log(numbers)
+  );
+};
+
+asyncSum(asyncNum1, Promise.reject(new Error("err")), asyncNum3).then(
+  (result) => console.log(result)
+);
