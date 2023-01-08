b2fb0ffe0bd6e6a37f84409d6be36d0e3dbbadd0
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2023-01-08 15:56:33 +0200

    JS Full lessons

--- lesson-36-promise-advanced-async-await/task2/gateway.js
+++ lesson-36-promise-advanced-async-await/task2/gateway.js
@@ -1,16 +1,17 @@
-export const fetchUserData = (userName) => {
-  return fetch(`https://api.github.com/users/${userName}`).then((response) => {
-    if (response.status === 200) {
-      return response.json();
-    }
-    throw new Error("Failed to load data");
-  });
+export const fetchUserData = async (userName) => {
+  const response = await fetch(`https://api.github.com/users/${userName}`);
+  if (response.ok) {
+    // eslint-disable-next-line no-return-await
+    return await response.json();
+  }
+  throw new Error("Failed to load data");
 };
 
-export const fetchRepositories = (url) =>
-  fetch(url).then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error("Failed to load data");
-  });
+export const fetchRepositories = async (url) => {
+  const response = await fetch(url);
+  if (response.ok) {
+    // eslint-disable-next-line no-return-await
+    return await response.json();
+  }
+  throw new Error("Failed to load data");
+};
